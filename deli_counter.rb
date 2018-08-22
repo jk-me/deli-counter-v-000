2584fe5fd26e0795b0897aaa8e85a7b1ee4a414e
Jenny K <jkam17@gmail.com>
2018-08-22 00:23:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,3 +7,19 @@ def line(array)
   else
     s = "The line is currently: "
     array.each {|n| s << "#{array.index(n)}. #{n}"
+  end
+end
+
+def take_a_number (array, name)
+  array << name 
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length==0 
+    puts"There is nobody waiting to be served!"
+  else
+    n = array.shift
+    puts"Currently serving #{n}."
+  end
+end
